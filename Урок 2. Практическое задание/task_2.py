3dd42cb6947175c6a94d9de0e7845f71291f762f
AntonBekker <antonbekkerspace@gmail.com>
2023-01-27 15:51:11 +0500

    Задание выполнено

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -13,8 +13,8 @@
 """
 my_list = input("Введите целые числа через пробел: ").split(' ')
 i, j = 0, 1
-while len(my_lst) > j:
-    my_lst[i], my_lst[j] = my_lst[j], my_lst[i]
+while len(my_list) > j:
+    my_list[i], my_list[j] = my_list[j], my_list[i]
     i += 2
     j += 2
-print('Результат:', *my_lst)
+print('Результат:', *my_list)
