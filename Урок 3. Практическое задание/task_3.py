f7c69c3d0e3bf5b41920f2fbc1f9624be19863fe
AntonBekker <antonbekkerspace@gmail.com>
2023-01-31 17:15:17 +0500

    Задание выполнено

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,25 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+""""Решение 1:""""
+def my_func():
+    agr1 = int(input("Ведите первое число: "))
+    arg2 = int(input("Ведите второе число: "))
+    arg3 = int(input("Ведите третье число: "))
+    my_list = [agr1,arg2,arg3]
+    my_list.sort(reverse=True)
+    return my_list
+listgoal = my_func()
+print(listgoal[0]+listgoal[1])
+
+""""Решение 2:""""
+def my_func():
+    agr1 = int(input("Ведите первое число: "))
+    arg2 = int(input("Ведите второе число: "))
+    arg3 = int(input("Ведите третье число: "))
+    my_list = [agr1,arg2,arg3]
+    return my_list
+listgoal = my_func()
+goal = sum(listgoal) - min(listgoal)
+print(goal)
+
