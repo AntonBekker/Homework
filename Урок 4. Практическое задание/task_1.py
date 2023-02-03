f2cc8b1b5b1248d760b7ffee5fd056c8138f0d93
AntonBekker <antonbekkerspace@gmail.com>
2023-02-03 16:41:41 +0500

    Задание выполнено

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,13 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+import sys
+f_obj, name_emp, rate_emp, hours_emp = sys.argv
+print(f_obj)
+
+def calculate_salary(rate,hours):
+    print(f'Сотрудник {name_emp} заработал {int(rate)*int(hours)} * 1.25')
+
+
+calculate_salary(rate_emp,hours_emp)
+
